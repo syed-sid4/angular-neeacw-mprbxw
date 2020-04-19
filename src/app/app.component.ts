d44f83d7e01c6b79bf69d608810843ad19d6e4a3
syed-sid4 <syed.syedbasha4@gmail.com>
2020-04-19 14:09:20 +0530

    1 file modified5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ export class AppComponent  {}
 
 
 /*
-Copyright Google LLC. All Rights Reserved.
+Copyright Google LLC. All Rights Reserved..
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
 */
